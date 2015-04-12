8f806cd1fab8c969be8c323097701081b2505218
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-04-12 12:04:15 +0300

    Initial commit

---
+++ Temp1/src/Node.java
@@ -0,0 +1,35 @@
+
+public class Node<T> {
+
+	
+    T data;
+
+    public Node(T data) { this.data = data; }
+
+    public void setData(T data) {
+        System.out.println(this.getClass().getName());
+        this.data = data;
+    }
+	
+	
+	public static void main(String[] args) {
+		
+		@SafeVarargs
+		Node<String>[] n=new Node<String>[10];
+		
+		
+		
+		
+		
+}
+
+}	
+
+class MyNode extends Node<Integer> {
+    public MyNode(Integer data) { super(data); }
+
+    public void setData(Integer data) {
+        System.out.println("MyNode.setData");
+        super.setData(data);
+    }
+}
