42d1a5f4eb663b5c511aaaa0fd0160606fd25882
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-04-27 14:38:55 +0300

    1

--- helloworldstruts2/src/helloworldstruts2/HelloWorldAction.java
+++ helloworldstruts2/src/helloworldstruts2/HelloWorldAction.java
@@ -1,5 +1,19 @@
 package helloworldstruts2;
 
 public class HelloWorldAction {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String execute(){
+		return "success";
+	}
 
 }
