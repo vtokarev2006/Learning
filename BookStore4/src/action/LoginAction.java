6ad926d8c4588bc76ab1a3031fde459dccbd4e03
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-04-27 17:07:14 +0300

    1

--- BookStore4/src/action/LoginAction.java
+++ BookStore4/src/action/LoginAction.java
@@ -1,5 +1,39 @@
 package action;
 
-public class LoginAction {
+import com.opensymphony.xwork2.ActionSupport;
 
+public class LoginAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+	private String username;
+	private String password;
+	
+	public String execute(){
+		if (getUsername().equals("kpss") && getPassword().equals("123")) {
+			return "success";
+		} else {
+			addActionError(getText("error.login"));
+			return "error";
+		}
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
