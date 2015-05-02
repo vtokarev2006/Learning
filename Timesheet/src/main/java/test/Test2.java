8472f598788528cc4c3118ca9af2ec4377d88de9
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-05-02 19:09:33 +0300

    1

---
+++ Timesheet/src/main/java/test/Test2.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class Test2 extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		entity.Agency agency = new entity.Agency();
+		
+		
+	}
+	
+	
+}
