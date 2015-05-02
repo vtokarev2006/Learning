abd67f003b251fe76556d5431760682b44659a19
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-05-02 18:18:14 +0300

    1

---
+++ Timesheet/src/main/java/entity/Calendar.java
@@ -0,0 +1,39 @@
+package entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Calendar {
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate date;
+	private boolean isDayOff;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public LocalDate getDate() {
+		return date;
+	}
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	public boolean isDayOff() {
+		return isDayOff;
+	}
+	public void setDayOff(boolean isDayOff) {
+		this.isDayOff = isDayOff;
+	}
+
+	
+}
--- Timesheet/src/main/java/entity/User.java
+++ Timesheet/src/main/java/entity/User.java
@@ -1,5 +1,7 @@
 package entity;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +16,13 @@ public class User {
 	private String lastName;
 	private String userName;
 	private String password;
+	private String email;
+	private HfmCode hfmCode;
+	private Position position;
+	private Department department;
+	private Agency agency;
+	private LocalDate hireDate;
+	private LocalDate leaveDate;
 	public Long getId() {
 		return id;
 	}
@@ -43,6 +52,48 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public HfmCode getHfmCode() {
+		return hfmCode;
+	}
+	public void setHfmCode(HfmCode hfmCode) {
+		this.hfmCode = hfmCode;
+	}
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Agency getAgency() {
+		return agency;
+	}
+	public void setAgency(Agency agency) {
+		this.agency = agency;
+	}
+	public LocalDate getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(LocalDate hireDate) {
+		this.hireDate = hireDate;
+	}
+	public LocalDate getLeaveDate() {
+		return leaveDate;
+	}
+	public void setLeaveDate(LocalDate leaveDate) {
+		this.leaveDate = leaveDate;
 	}
 	
 
