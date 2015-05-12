1027c1e9551bb3ec035fbd89364358d4d973c114
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-05-12 15:05:27 +0300

    1

--- Timesheet/src/main/java/entity/SecurityGroup.java
+++ Timesheet/src/main/java/entity/SecurityGroup.java
@@ -19,6 +19,30 @@ public class SecurityGroup {
 	@ManyToMany(fetch=FetchType.EAGER, mappedBy="securityGroups")
 	private Set<User> user;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<User> getUser() {
+		return user;
+	}
+
+	public void setUser(Set<User> user) {
+		this.user = user;
+	}
+
 
 	
 	
