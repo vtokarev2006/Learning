f9a09f97960a34eb6834aff398b467fe84e7f22b
vtokarev2006 <vyacheslav.tokarev@gmail.com>
2015-05-18 19:17:44 +0300

    1

---
+++ task-time-tracker/src/test/java/dao/AbstractDaoForTesting.java
@@ -0,0 +1,25 @@
+package dao;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+
+@ContextConfiguration("/testingContext.xml")
+public abstract class AbstractDaoForTesting extends AbstractTransactionalJUnit4SpringContextTests {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired(required = true)
+    protected CompanyDao companyDao;
+    @Autowired(required = true)
+    protected ProjectDao projectDao;
+    @Autowired(required = true)
+    protected TaskDao taskDao;
+    @Autowired(required = true)
+    protected UserDao userDao;
+    @Autowired(required = true)
+    protected TaskLogDao taskLogDao;	
+	
+
+}
